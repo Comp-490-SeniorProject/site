992f660069f9e64634a96f54a1769832e4147aec
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2021-10-29 14:55:08 -0700

    Set up PostgreSQL with Django
    Add a db service to Docker Compose which uses the official PostgreSQL
    image. By default, SQLite will still be used. However, if running
    through Docker Compose, PostgreSQL will be used instead. The database
    connection string can be changed via the DATABASE_URL environment
    variable.
    
    Configure the web service in Docker Compose to wait for PostgreSQL to
    be ready before starting the Django server. Otherwise, the latter may
    try and fail to connect to the former before it's ready.

--- web/settings.py
+++ web/settings.py
@@ -99,10 +99,7 @@ WSGI_APPLICATION = "web.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db(default=f"sqlite:////{BASE_DIR / 'db.sqlite3'}"),
 }
 
 
