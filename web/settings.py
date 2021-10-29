70280709c48dc92e19e704a044c293c7dddc7dd3
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2021-10-29 15:32:26 -0700

    Make default static root relative to base dir
    This setting was hard-coded to the working directory used in the Docker
    image. However, polluting the root directory locally is not good, so
    change this to be relative to the configured BASE_DIR. Also, rename
    the directory so that it lines up with what was already in .gitignore.

--- web/settings.py
+++ web/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = env("STATIC_ROOT", default="/web/staticfiles")
+STATIC_ROOT = env("STATIC_ROOT", default=BASE_DIR / "static_root")
 
 WEBPACK_LOADER = {
     "DEFAULT": {
