ea12fc531f8089f27d07a8b3d734519d45d27db6
Alina <fforinfo@gmail.com>
2021-11-17 22:08:08 +0100

    Fixing prettier linting

--- web/frontend/angular/extra-webpack.config.js
+++ web/frontend/angular/extra-webpack.config.js
@@ -1,12 +1,10 @@
-const path = require('path');
-const BundleTracker = require('webpack-bundle-tracker');
+const path = require("path");
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    output: {
-        path: path.resolve('../static'),
-        filename: "[name]-[contenthash].js"
-    },
-    plugins: [
-        new BundleTracker({filename: './webpack-stats.json'})
-    ]
+  output: {
+    path: path.resolve("../static"),
+    filename: "[name]-[contenthash].js",
+  },
+  plugins: [new BundleTracker({ filename: "./webpack-stats.json" })],
 };
