290a23bf25cd0de8f7a64ef2f674e3fc5f31c571
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2021-11-26 19:25:38 -0800

    Add a parameter model

--- web/api/models/__init__.py
+++ web/api/models/__init__.py
@@ -1,4 +1,5 @@
 from .device import Device  # noqa: F401
+from .parameter import Parameter  # noqa: F401
 from .test import Test  # noqa: F401
 from .test_history import TestHistory, TestStatus  # noqa: F401
 from .user import User  # noqa: F401
