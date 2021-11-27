df57127f24d001cdee1e9a7054411da55d889139
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2021-11-26 19:15:47 -0800

    Add constraints to test history model
    Check the nullity of the timestamp fields depending on the status.

--- web/api/models/test_history.py
+++ web/api/models/test_history.py
@@ -3,13 +3,15 @@ from django.db import models
 from . import Test
 
 
+class TestStatus(models.TextChoices):
+    SCHEDULED = "SC"
+    CANCELLED = "CA"
+    RUNNING = "RU"
+    SUCCEEDED = "SU"
+    FAILED = "FA"
+
+
 class TestHistory(models.Model):
-    class Status(models.TextChoices):
-        SCHEDULED = "SC"
-        CANCELLED = "CA"
-        RUNNING = "RU"
-        SUCCEEDED = "SU"
-        FAILED = "FA"
 
     test = models.ForeignKey(
         Test, on_delete=models.CASCADE, help_text="The test to which this history pertains."
@@ -22,7 +24,39 @@ class TestHistory(models.Model):
     )
     status = models.CharField(
         max_length=2,
-        choices=Status.choices,
-        default=Status.SCHEDULED,
+        choices=TestStatus.choices,
+        default=TestStatus.SCHEDULED,
         help_text="The current status of this test instance's execution.",
     )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="scheduled_timestamps_null",
+                check=models.Q(
+                    status__exact=TestStatus.SCHEDULED,
+                    started_at__isnull=True,
+                    ended_at__isnull=True,
+                ),
+            ),
+            models.CheckConstraint(
+                name="running_started_at_not_null_ended_at_null",
+                check=models.Q(
+                    status__exact=TestStatus.RUNNING,
+                    started_at__isnull=False,
+                    ended_at__isnull=True,
+                ),
+            ),
+            models.CheckConstraint(
+                name="succeeded_timestamps_not_null",
+                check=models.Q(
+                    status__exact=TestStatus.SUCCEEDED,
+                    started_at__isnull=False,
+                    ended_at__isnull=False,
+                ),
+            ),
+            models.CheckConstraint(
+                name="cancelled_ended_at_not_null",
+                check=models.Q(status__exact=TestStatus.CANCELLED, ended_at__isnull=False),
+            ),
+        ]
