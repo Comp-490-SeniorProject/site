d1b6ad8b66aca3f013dbfe8a403ed0c37fb1eaa1
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2021-11-30 12:21:43 -0800

    Add a model for notification settings

--- web/api/models/__init__.py
+++ web/api/models/__init__.py
@@ -1,4 +1,5 @@
 from .device import Device  # noqa: F401
+from .notification_settings import NotificationSettings  # noqa: F401
 from .parameter import Parameter  # noqa: F401
 from .test import Test  # noqa: F401
 from .test_history import TestHistory, TestStatus  # noqa: F401
