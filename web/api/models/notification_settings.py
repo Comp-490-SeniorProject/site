fa0b9d3870bdd615e26581e43e3fe8b122e9b7c7
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2021-11-30 13:27:14 -0800

    Use JsonLogic for the notification condition
    Validate the JSON can be parsed by checking if an exception is raised
    during parsing. Unfortunately, the library doesn't use a custom
    exception type, so the validator must do a broad catch for exceptions.

--- web/api/models/notification_settings.py
+++ web/api/models/notification_settings.py
@@ -1,8 +1,17 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from json_logic import jsonLogic
 
 from web.utils import fields
 
 from . import Test
+
+
+def validate_json_logic(value):
+    try:
+        jsonLogic(value)
+    except Exception as e:
+        raise ValidationError(getattr(e, "message", str(e)), params=dict(value=value))
 
 
 class NotificationDest(models.TextChoices):
@@ -14,8 +23,9 @@ class NotificationSettings(models.Model):
     test = models.ForeignKey(
         Test, on_delete=models.CASCADE, help_text="The test for which to notify."
     )
-    condition = models.TextField(
-        help_text="An expression to evaluate to determine whether to notify."
+    condition = models.JSONField(
+        validators=(validate_json_logic,),
+        help_text="A JsonLogic-encoded expression to evaluate to determine whether to notify.",
     )
     message = models.TextField(blank=True, help_text="The message to display in the notification.")
     destination = fields.ChoiceArrayField(
