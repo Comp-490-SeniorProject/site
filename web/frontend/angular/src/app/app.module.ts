e4ff505ae0e0138ec7d4fbea4effd2ff72224ce4
mndzamel <79746368+mndzamel@users.noreply.github.com>
2021-12-02 12:13:15 -0800

    Updated app.module.ts
    Commented out a few lines to reflect the change made in a recent commit in this branch (removal of AuthComponent). This allows the code to compile.

--- web/frontend/angular/src/app/app.module.ts
+++ web/frontend/angular/src/app/app.module.ts
@@ -8,13 +8,14 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ProductComponent } from './product/product.component';
 import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
+//import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin/admin.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, AboutComponent, ProductComponent, HomeComponent, AuthComponent, AdminComponent, SignInComponent
+    //AppComponent, HeaderComponent, FooterComponent, AboutComponent, ProductComponent, HomeComponent, AuthComponent, AdminComponent, SignInComponent
+    AppComponent, HeaderComponent, FooterComponent, AboutComponent, ProductComponent, HomeComponent, AdminComponent, SignInComponent
   ],
   imports: [
     BrowserModule,
