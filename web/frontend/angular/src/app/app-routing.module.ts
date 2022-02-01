9fb9799985699a0349c4456a90db1f9acf3386ef
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-01-31 18:18:18 -0800

    Apply eslint & prettier formatting

--- web/frontend/angular/src/app/app-routing.module.ts
+++ web/frontend/angular/src/app/app-routing.module.ts
@@ -1,42 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from "@angular/core"
+import {RouterModule, Routes} from "@angular/router"
 
-import { AboutComponent } from './about/about.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import {AboutComponent} from "./about/about.component"
+import {FooterComponent} from "./footer/footer.component"
+import {HeaderComponent} from "./header/header.component"
+import {HomeComponent} from "./home/home.component"
+import {ProductComponent} from "./product/product.component"
+import {SignInComponent} from "./sign-in/sign-in.component"
 
-import { AdminComponent } from './admin/admin.component';
+import {AdminComponent} from "./admin/admin.component"
 //import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth.guard';
-
+import {AuthGuard} from "./auth.guard"
 
 const routes: Routes = [
-  { path: '',
-  pathMatch: 'full',
-  component: HomeComponent},
-  { path: 'app-header',
-  component: HeaderComponent},
-  { path: 'app-footer',
-  component: FooterComponent},
-  { path: 'app-about',
-  component: AboutComponent},
-  { path: 'app-product',
-  component: ProductComponent},
-  { path: 'app-home',
-  component: HomeComponent},
-  //{ path: 'app-auth',
-  //component: AuthComponent},
-  { path: 'app-admin',
-  component: AdminComponent, canActivate: [AuthGuard]},
-  { path: 'app-sign-in',
-  component: SignInComponent},
-];
+    {path: "", pathMatch: "full", component: HomeComponent},
+    {path: "app-header", component: HeaderComponent},
+    {path: "app-footer", component: FooterComponent},
+    {path: "app-about", component: AboutComponent},
+    {path: "app-product", component: ProductComponent},
+    {path: "app-home", component: HomeComponent},
+    //{ path: 'app-auth',
+    //component: AuthComponent},
+    {path: "app-admin", component: AdminComponent, canActivate: [AuthGuard]},
+    {path: "app-sign-in", component: SignInComponent},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
