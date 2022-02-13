913b1b8cb87c4b55491ddcd6994a85cf05c06526
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-12 17:23:06 -0800

    Fix malformed permission policy header
    Seems to be caused by secure's default value using a semicolon delimiter
    instead of a comma. It is fixed by secure in an unreleased version,
    so the value is manually overwritten for now.

--- web/middleware.py
+++ web/middleware.py
@@ -10,7 +10,23 @@ csp_policy = (
     .font_src("'self'", "fonts.gstatic.com")
     .style_src("'self'", "'unsafe-inline'", "fonts.googleapis.com")
 )
-secure_headers = secure.Secure(csp=csp_policy, permissions=secure.PermissionsPolicy())
+
+# Secure 0.3.0 mistakenly has a semicolon in the default value,
+# which is fixed in an unreleased version.
+permissions_policy = secure.PermissionsPolicy()
+permissions_policy.value = (
+    "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), "
+    "camera=(), clipboard-read=(), clipboard-write=(), cross-origin-isolated=(), "
+    "display-capture=(), document-domain=(), encrypted-media=(), "
+    "execution-while-not-rendered=(), execution-while-out-of-viewport=(), "
+    "fullscreen=(), gamepad=(), geolocation=(), gyroscope=(), magnetometer=(), "
+    "microphone=(), midi=(), navigation-override=(), payment=(), "
+    "picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), "
+    "speaker-selection=(), sync-xhr=(), usb=(), web-share=(), "
+    "xr-spatial-tracking=()"
+)
+
+secure_headers = secure.Secure(csp=csp_policy, permissions=permissions_policy)
 
 
 def set_secure_headers(get_response):
