1b4d90e4f5142dc1c8bc68b010b8c9557040068c
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-12 17:20:35 -0800

    Fix CSP errors
    Adjust CSP to meet Angular's minimum requirements. Also allow Google
    domains related to fonts.

--- web/middleware.py
+++ web/middleware.py
@@ -2,16 +2,13 @@ import secure
 
 __all__ = ("set_secure_headers",)
 
+# Angular requires default-src 'self'; style-src 'self' 'unsafe-inline';
+# https://angular.io/guide/security#content-security-policy
 csp_policy = (
     secure.ContentSecurityPolicy()
-    .default_src("'none'")
-    .base_uri("'self'")
-    .connect_src("'self'")
-    .form_action("'self'")
-    .frame_ancestors("'self'")
-    .frame_src("'none'")
-    .style_src("'self'")
-    .img_src("'self'")
+    .default_src("'self'")
+    .font_src("'self'", "fonts.gstatic.com")
+    .style_src("'self'", "'unsafe-inline'", "fonts.googleapis.com")
 )
 secure_headers = secure.Secure(csp=csp_policy, permissions=secure.PermissionsPolicy())
 
