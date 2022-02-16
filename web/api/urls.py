0a16165489b23ec8f61437990e87e05b0121ef51
Mark <1515135+MarkKoz@users.noreply.github.com>
2022-02-16 15:50:36 -0800

    Route the API endpoints (#39)

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,7 +1,22 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .viewsets import (
+    DeviceViewSet,
+    NotificationSettingsViewSet,
+    ParameterViewSet,
+    TestHistoryViewSet,
+    TestViewSet,
+)
+
 router = routers.DefaultRouter()
+router.register("devices", DeviceViewSet, basename="device")
+router.register(
+    "notification_settings", NotificationSettingsViewSet, basename="notification_settings"
+)
+router.register("parameters", ParameterViewSet, basename="parameter")
+router.register("tests", TestViewSet, basename="test")
+router.register("test_history", TestHistoryViewSet, basename="test_history")
 
 urlpatterns = [
     path("", include(router.urls)),
