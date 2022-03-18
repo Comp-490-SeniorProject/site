d29c620bcbc385151c50c6df406d64a894d7ffd5
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-03-17 19:14:29 -0700

    Store sensor ID in parameter
    The backend needs to know the sensor's ID, so it can tell the device
    when sensor to test.

--- web/api/models/parameter.py
+++ web/api/models/parameter.py
@@ -24,9 +24,25 @@ class Parameter(models.Model):
         choices=ParameterType.choices,
         help_text="Determines how the parameter is tested and how its data is collected.",
     )
+    sensor_id = models.PositiveSmallIntegerField(
+        null=True, help_text="The ID of the sensor which corresponds to this parameter."
+    )
     device = models.ForeignKey(
         Device,
         on_delete=models.CASCADE,
         related_name="parameters",
         help_text="The device which tests this parameter.",
     )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="auto_sensor_not_null",
+                check=models.Q(type="man") | models.Q(sensor_id__isnull=False),
+            ),
+            models.UniqueConstraint(
+                name="unique_device_sensor",
+                fields=["sensor_id", "device"],
+                condition=models.Q(sensor_id__isnull=False),
+            ),
+        ]
