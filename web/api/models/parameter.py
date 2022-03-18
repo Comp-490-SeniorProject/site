78811af12610df8baf4d3aa1f52b5a75c64ef4fc
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-03-17 19:12:13 -0700

    Add type to parameter
    Support auto, manual, and semi-auto (if user intervention required).

--- web/api/models/parameter.py
+++ web/api/models/parameter.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from .test import Test
+
+
+class ParameterType(models.TextChoices):
+    AUTO = "auto", _("Automatic")
+    MANUAL = "man", _("Manual")
+    SEMI = "semi", _("Semi-automatic")
 
 
 class Parameter(models.Model):
@@ -8,9 +15,10 @@ class Parameter(models.Model):
     name = models.TextField(help_text="A user-friendly string that names the parameter.")
     description = models.TextField(blank=True, help_text="A description of the parameter.")
     unit = models.TextField(help_text="The name of the unit of measurement for the parameter.")
-    is_manual = models.BooleanField(
-        default=False,
-        help_text="Whether the parameter has to be set manually by a user after a test run.",
+    type = models.CharField(
+        max_length=4,
+        choices=ParameterType.choices,
+        help_text="Determines how the parameter is tested and how its data is collected.",
     )
     test = models.ForeignKey(
         Test, on_delete=models.CASCADE, help_text="The test which sets this parameter."
