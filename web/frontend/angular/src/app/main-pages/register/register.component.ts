468494fbd90182a2e855fde4c4d02071bd43a895
alinaah <fforinfo@gmail.com>
2022-03-26 09:45:38 +0530

    error visibility changed and jquery removed

--- web/frontend/angular/src/app/main-pages/register/register.component.ts
+++ web/frontend/angular/src/app/main-pages/register/register.component.ts
@@ -4,7 +4,6 @@ import {Validators} from "@angular/forms"
 import {FormBuilder} from "@angular/forms"
 import {Router} from "@angular/router"
 import {AuthService} from "src/app/auth/auth.service"
-import * as $ from "jquery"
 
 @Component({
     selector: "app-register",
@@ -46,12 +45,12 @@ export class RegisterComponent implements OnInit {
             },
             (error) => {
                 console.log(error)
-                $("input[type=email],input[type=password]").addClass("error")
-                $("#error").removeClass("d-none")
-                setTimeout(() => {
-                    $("input").removeClass("error")
-                    $("#error").addClass("d-none")
-                }, 2000)
+                let input = document.getElementsByTagName("input")
+                for (let i = 0; i < input.length - 1; i++) {
+                    input[i].setAttribute("class", "error form-control")
+                }
+                let err = document.getElementById("error")
+                err?.classList.remove("d-none")
             }
         )
     }
