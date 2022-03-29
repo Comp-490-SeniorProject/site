fa31b8af15b07f78b38837433f9794296f51e0d1
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-03-29 16:37:21 -0700

    Support creating/updating the schedule field via TestSerializer

--- web/api/serializers/test.py
+++ web/api/serializers/test.py
@@ -1,9 +1,30 @@
+from django_q.models import Schedule
 from rest_framework import serializers
 
 from web.api.models import Test
 
 
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = ["schedule_type", "minutes", "next_run"]
+
+
 class TestSerializer(serializers.ModelSerializer):
+    schedule = ScheduleSerializer(many=False)
+
     class Meta:
         model = Test
         fields = "__all__"
+
+    def create(self, validated_data):
+        schedule = Schedule.objects.create(
+            func="print",
+            args=f"'Running test {validated_data['name']} for param {validated_data['parameter']}'",
+            **validated_data.pop("schedule"),
+        )
+        return Test.objects.create(**validated_data, schedule=schedule)
+
+    def update(self, instance, validated_data):
+        self.schedule.update(instance.schedule, validated_data.pop("schedule"))
+        return super().update(instance, validated_data)
