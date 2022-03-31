8fef6a97c528d2ce8c3aa31cff1d9f29378b8176
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-03-31 16:20:36 -0700

    Update filter and ordering fields of TestViewSet

--- web/api/viewsets/test.py
+++ web/api/viewsets/test.py
@@ -10,15 +10,15 @@ class TestViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = [
         "name",
-        "created_at",
-        "frequency",
         "parameter__id",
         "parameter__type",
         "parameter__sensor_id",
         "parameter__device__id",
+        "schedule__next_run",
+        "schedule__schedule_type",
     ]
-    ordering_fields = ["created_at"]
-    ordering = ["created_at"]
+    ordering_fields = ["schedule__next_run"]
+    ordering = ["schedule__next_run"]
 
     def get_queryset(self):
         return Test.objects.filter(parameter__device__owner=self.request.user)
